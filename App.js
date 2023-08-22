5b5aa4620459eb909dcaf59acf0fac582dfb41ca
coderam95 <ramdev100495@gmail.com>
2023-08-22 10:40:41 +0530

    episode-1

--- App.js
+++
@@ -1,7 +0,0 @@
-const heading = React.createElement("div", {id: "heading"}, React.createElement("div", {id: "child"}, 
-[React.createElement("h1",{},"I am the react nested element"),React.createElement("h2",{},"I am the sibling to the first element")]),
-React.createElement("div", {id: "child2"}, 
-[React.createElement("h1",{},"I am the react nested element"),React.createElement("h2",{},"I am the sibling to the first element")]));
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
